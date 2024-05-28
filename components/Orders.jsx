42a11a07a268d375f0060caed095915b22fc9a57
Md.Arifur Rahman sajid <159644399+arifurrahmansajid@users.noreply.github.com>
2024-05-28 15:11:51 +0600

    Add files via upload

---
+++ components/Orders.jsx
@@ -0,0 +1,54 @@
+
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>Your personal order history</h2>
+            <ul className="grid md:grid-cols-2 sm:grid-cols-1 justify-center items-center">
+                <li><div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div></li>
+                <li><div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div></li>
+                <li><div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div></li>
+                <li><div className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div></li>
+                
+            </ul>
+        </div>
+    );
+};
+
+export default Orders;
